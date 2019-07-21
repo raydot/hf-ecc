fea1e75f560d43fc175da544aa137a926a89bda9
Dave Kanter <moatgoat@hotmail.com>
2019-07-21 14:58:58 -0700

    All of the text is flowing into cards from the JSON.  Flipping and filtering left.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,43 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app" class="foo">
+      <h1>Test Results</h1>
+      <app-cards :cardData="cardData" />
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import AppCards from '@/components/AppCards.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  export default {
+    name: 'app',
+    components: {
+      AppCards,
+    },
+    data() {
+      return {
+        cardData: []
+      }
+    },
+    mounted() {
+      this.getCards()
+    },
+    methods: {
+      async getCards() {
+        try {
+          const response = await fetch('https://s3-us-west-1.amazonaws.com/hero-engineering-public/interview/fe-code-challenge.json')
+          //const response = await fetch('https://jsonplaceholder.typicode.com/users')
+          const data = await response.json()
+          this.cardData = data.cards
+          //console.log(this.cardData)
+        } catch(error) {
+          // eslint-disable-next-line
+          console.error(error) // Or somehow handle the error.
+        }
+      },
+    }
   }
-}
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+
 </style>
---
+++ src/components/AppCards.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="card-grid">
+          <p v-if="cardData.length < 1" class="no-card-data-found">
+              No card data loaded.
+            </p>
+        <div class="card--body" v-for="card in cardData" :key="card.id">
+          <img :src="card.image" v-bind:alt="`${ card.title }`">
+          <p class="card--body__title">{{ card.title }}</p>
+          <p class="card--body__description">{{ card.description | truncate(200, '...') }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'app-cards',
+        props: {
+            cardData: Array
+        },
+        data() {
+            return {}
+        },
+        methods: {
+
+        },
+        filters: {
+            truncate: function (text, length, suffix) {
+                return text.substring(0, length) + suffix
+            },
+        },
+    }
+</script>
+
+<style scoped>
+    .card-grid {
+        font-family: Arial, Helvetica, sans-serif;
+        color: #000;
+        text-align: center;
+    }
+
+    .card--body {
+        height: 280px;
+        width: 215px;
+        border: 1px solid #f00;
+        overflow: hidden;
+    }
+
+    .card--body__title {
+        font-size: 1em;
+        font-weight: 700;
+    }
+
+    .card--body__description {
+        font-size: .75em;
+        text-align: left;
+        overflow: hidden;
+        hyphens: auto;
+        text-overflow: ellipsis;
+        margin: 10px;
+        line-clamp: 6;
+        -webkit-line-clamp: 6;
+    }
+
+
+    img {
+        height: 100px;
+    }
+</style>
